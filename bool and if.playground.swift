cefc8026aa5cd6a06cd5ad1e5ccfa2d6d0ac1a15
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2020-10-27 19:13:47 -0500

    Create bool and if.playground.swift

---
+++ bool and if.playground.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+var EstudiosAndres = "Grado Secundario"
+var salario = 0
+var cedulaProfesional = false
+var cedulamaestrai = true
+
+if cedulaProfesional{
+    salario = 9000
+}else{
+    salario = 8000
+}
