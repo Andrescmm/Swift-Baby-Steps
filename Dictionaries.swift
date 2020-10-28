2d233b5d2e4d16dd08182b335436306aecafd4f4
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2020-10-27 21:50:06 -0500

    Create Dictionaries.swift

---
+++ Dictionaries.swift
@@ -0,0 +1,27 @@
+
+import UIKit
+
+// Sintaxis diccionarios
+
+let classicDictionary = Dictionary<Int, String>()
+
+// Mejor manera
+var myModernDictionary = [Int:String]()
+
+// Add elements
+myModernDictionary = [001:"Andres",002:"Ariana"]
+
+// Add only one elment
+myModernDictionary[003] = "Paola"
+
+
+// Access to a value
+myModernDictionary[002]
+
+// La clave del diccionario es unica
+
+
+// Actualizar valores
+myModernDictionary.updateValue("Peter", forKey: 002)
+
+print(myModernDictionary)
