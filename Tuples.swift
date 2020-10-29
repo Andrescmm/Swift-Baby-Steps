64bd99a7dbafec1571b1715812c716db1974cd30
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2020-10-29 18:59:29 -0500

    Create Tuples.swift

---
+++ Tuples.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Nombre ,Apellido , edad, estatura
+
+var persona = ("Andres","Cusirramos",19,1.80)
+
+persona.0
+persona.1
+persona.3
+
+
+// asignando vlaores de la tupla
+var ( Nombre ,Apellido , edad, estatura) = persona
+
+Nombre
+edad
+
+
+//asignar nombre a cada miembro de la tupla
+var peoplenamedtuple = (nombre:"Andres",apellido:"Cusirramos",19,1.80)
+
+peoplenamedtuple.nombre
