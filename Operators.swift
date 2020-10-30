daf5fa2cc03766f0b668cb14bfd6503bb7458ffe
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2020-10-29 19:54:36 -0500

    Create Operators.swift

---
+++ Operators.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// Operador de Asignacion
+
+let numeroConstante = 18
+
+// Operadores Aritmeticos
+
+var numeroVariable = 2
+
+numeroVariable+numeroConstante
+
+
+
+// Operadores Compuestos
+
+// numero + numero = 2
+numeroVariable += 2
+
+numeroVariable -= 2
+
+numeroVariable *= 2
+
+numeroVariable /= 2
+
+
+
+// Operadores de Comparacion
+
+// == igual a
+// != diferente
+// menor <
+// mayor >
+// <=
+// >=
