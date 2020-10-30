5639519761e025ac4b3d7054d737db87c050a865
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2020-10-29 19:41:20 -0500

    Create Range Operator.swift

---
+++ Range Operator.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var ArregloEjemplo = [0,1,2,3,4,5,6,7,8,9]
+
+// Two-sided Range Operator
+
+var subsetarray = ArregloEjemplo[1...3]  // ...<
+
+// One-sided Range Operator
+
+var subSetArray2 = ArregloEjemplo[...3]  // 5...
+
+var rangoCerrado = [1...5] // Solo contiene un valor
+
+rangoCerrado[0]
